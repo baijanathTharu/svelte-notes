e932310129e07f2255371f7edb3aca1e1efd4b3f
baijanathTharu <bnchaudhary.tharu@gmail.com>
2023-01-19 21:50:16 +0545

    create navbar

---
+++ src/components/navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
+</script>
+
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			Svelte Notes
+		</span>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		<NavLi href="/">Notes</NavLi>
+		<NavLi href="/create">Create</NavLi>
+	</NavUl>
+</Navbar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
+	import Navbar from '$components/navbar.svelte';
 	import '../app.postcss';
 </script>
 
+<Navbar />
 <slot />
